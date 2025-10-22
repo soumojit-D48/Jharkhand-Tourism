afcecdd91e98bdf4f4025489c00c1344fe322d89
soumojit-D48 <soumojitdas107@gmail.com>
2025-10-22 18:55:01 +0530

    edit hotel feat

--- server/config/corsConfig.js
+++ server/config/corsConfig.js
@@ -3,8 +3,8 @@ function cofigureCors() {
     return ({
         origin: (origin, callBack) => {
             const allowOrigins = [
-                'http://localhost:5173', // dev
-                // 'https://eco-quest-7p8q.onrender.com', // production domain
+                // 'http://localhost:5173', // dev
+                'https://eco-quest-7p8q.onrender.com', // production domain
             ]
 
             if(!origin || allowOrigins.indexOf(origin) !== -1){
